f3d03d064179230760ae7900d6e04dc2526d709c
dan7sc <su7cheng@gmail.com>
2021-08-25 16:25:01 -0300

    Initial test: true is true

---
+++ test/src/home/home_page_test.dart
@@ -0,0 +1,10 @@
+import 'package:aula_textfield2/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('home page ...', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(true, true);
+  });
+}
